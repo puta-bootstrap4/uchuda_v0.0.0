db2ec93e2c43c3de3faa5337c29374f15d304305
kawanokatsuyuki <katsuyuki05170517@gmail.com>
2024-05-19 01:29:36 +0900

    correct2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,13 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <meta name="google-adsense-account" content="ca-pub-4549864067149386"></meta>
       <body className={inter.className}>{children}
+
       <Script src="https://aframe.io/releases/1.5.0/aframe.min.js" strategy="beforeInteractive" />
       <Script src="https://aframe.io/releases/1.2.0/aframe.min.js" async />
       <Script src="https://unpkg.com/aframe-particle-system-component/dist/aframe-particle-system-component.min.js"  async/>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4549864067149386"
-     crossOrigin="anonymous" />
+
       </body>
       {/*bodyの下にaframe Script書いたらエラーブラウザのコンソールでエラーが見受けられる*/}
     </html>
