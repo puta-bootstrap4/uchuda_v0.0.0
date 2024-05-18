5a12ec181f5bb60af87600eb2ace24e483ba0fec
kawanokatsuyuki <katsuyuki05170517@gmail.com>
2024-05-19 01:27:27 +0900

    correct2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,8 @@ export default function RootLayout({
       <Script src="https://aframe.io/releases/1.5.0/aframe.min.js" strategy="beforeInteractive" />
       <Script src="https://aframe.io/releases/1.2.0/aframe.min.js" async />
       <Script src="https://unpkg.com/aframe-particle-system-component/dist/aframe-particle-system-component.min.js"  async/>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4549864067149386"
+     crossOrigin="anonymous" />
       </body>
       {/*bodyの下にaframe Script書いたらエラーブラウザのコンソールでエラーが見受けられる*/}
     </html>
